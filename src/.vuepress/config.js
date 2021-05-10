68c4a22a39c51a35644cba50ca00d94925e6910a
Rodrigo Peña <lrpena@miuandes.cl>
2021-05-10 00:34:15 -0400

    chore: change npm for yarn and add google analytics tracking

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -66,5 +66,11 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      '@vuepress/google-analytics',
+      {
+         'ga': 'G-5BV2X8V3P4'
+      }
+    ]
   ]
 }
